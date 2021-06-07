c92965ff8fb49d895a81d9bb5a74ec06ab9baa54
陈冬春 <734097921@qq.com>
2021-06-07 17:49:18 +0800

    composer andy test

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require "vendor/autoload.php";
+
+$Test = new Andy\Test();
+
+$Test->test();
---
+++ src/andy/Test.php
@@ -0,0 +1,8 @@
+<?php
+namespace Andy;
+
+class Test {
+	function test() {
+		echo "Form Test -> test(), By Andy";
+	}
+}
